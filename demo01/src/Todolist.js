665d3590eb90a0f3ea9da1b670788b140071b9a8
bufan <l>
2019-12-02 00:12:10 +0800

    wan

---
+++ demo01/src/Todolist.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const TodoList = (props) => {
+    let { inputValue, inputChange, clickButton, list, deleteItem } = props;
+    return (
+        <div>
+            <div>
+                <input
+                    value={inputValue}
+                    onChange={inputChange}
+                />
+                <button
+                    onClick={clickButton}
+                >添加</button>
+            </div>
+            <ul>
+                {
+                    list.map((item, index) => {
+                        return (<li key={index}
+                            onClick={() => { deleteItem(index) }}
+                        >{item}</li>)
+                    })
+                }
+            </ul>
+        </div>
+
+    );
+}
+
+const stateToProps = (state) => {
+    return {
+        inputValue: state.inputValue,
+        list: state.list
+    }
+}
+
+const dispatchToProps = (dispatch) => {
+    return {
+        inputChange(e) {
+            let action = {
+                type: 'change_input',
+                value: e.target.value
+            }
+            dispatch(action);
+        },
+
+        clickButton() {
+            let action = { type: 'add_item' }
+            dispatch(action);
+        },
+
+        deleteItem(index) {
+            let action = {
+                type: 'delete_item',
+                index
+            }
+            dispatch(action);
+        }
+    }
+}
+
+export default connect(stateToProps, dispatchToProps)(TodoList);
